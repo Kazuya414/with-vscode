424374327ae35c9f2d68bdc3b05d86141b0ca096
watanabe kazuya <k.watanabe.sys20@morijyobi.ac.jp>
2021-03-30 10:59:12 +0900

    メニューページの新規追加
    index.htmlからの遷移先ページとして
    menu.htmlを新規作成しました。

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app= Flask(__name__)
 @app.route("/")
 def top():
     return render_template("index.html")
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")    
 
 if __name__ == "__main__":
     app.run(debug=True)
